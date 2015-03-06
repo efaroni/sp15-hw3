e9f814267913880dea2236ec3db58c7d7db2ea47
Eduardo Faroni <e.faroni@berkeley.edu>
2015-03-05 18:04:28 -0800

    Made models display a little nicer, going to submit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,11 @@ class PagesController < ApplicationController
 		@user_models = User.all
 		@cat_models = Cat.all
     end
+    #def index
+    #	@users = User.all
+    #end
 end
+
+#<% @Users.each do |u| %>
+#	<p> <%= u.name%> </p>
+#<%end>
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   root 'pages#home'
+  #get "users" , to: "users#index"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
